44d5c283be1929ec35b1e57f81af846e024f3206
Pawan Kumar <pk.soft29@gmail.com>
2020-09-09 22:55:51 +0530

    Added: Arithmetic.sh added with Compute c+a/b

--- Arithmetic.sh
+++ Arithmetic.sh
@@ -10,3 +10,7 @@ echo "$num1 + $num2 * $num3 = $Compute1"
 Compute2=$(($num1 * $num2 + $num3))
 
 echo "$num1 * $num2 + $num3 = $Compute2"
+
+Compute3=`echo "scale=1;$num1 + $num2 / $num3" | bc`
+
+echo "$num1 + $num2 / $num3 = $Compute3"
